21d0f8749003ad82a01ebc3f9f72c256604c84a3
Jeevan Kumar <rg1kumar1@gmail.com>
2020-11-26 21:47:03 +0530

    Users added. Login, logout working. Styling needed

--- project_resurrect/settings.py
+++ project_resurrect/settings.py
@@ -81,6 +81,8 @@ DATABASES = {
     }
 }
 
+AUTH_USER_MODEL = "stock.User"
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
--- stock/views.py
+++ stock/views.py
@@ -1,7 +1,53 @@
-from django.http.response import Http404, HttpResponse
+from django.contrib.auth import authenticate, login, logout
+from django.http.response import Http404, HttpResponse, HttpResponseRedirect
+from django.db import IntegrityError
 from django.shortcuts import render
+from django.urls import reverse
+from .models import *
 
 # Create your views here.
 
 def index(request):
     return render(request, "stock/index.html")
+
+def login_view(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user_authenticate = authenticate(request, username=username, password=password)
+
+        if user_authenticate is not None:
+            login(request, user_authenticate)
+            return HttpResponseRedirect(reverse("index"))
+        else:
+            return render(request, "stock/login.html", {
+                'err_message': "Invalid Username or Password. Try again."
+            })
+    return render(request, "stock/login.html")
+
+def register_view(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        password_confirm = request.POST["password_confirm"]
+
+        if password != password_confirm:
+            return render(request, "stock/register.html", {
+                'err_message': "Password do not match. Try again."
+            })
+
+        try:
+            new_user = User.objects.create_user(username=username, password=password)
+            new_user.save()
+        except IntegrityError:
+            return render(request, "stock/register.html", {
+                'err_message': "Username already exists. Try again."
+            })
+        login(request, new_user)
+        return HttpResponseRedirect(reverse("index"))
+
+    return render(request, "stock/register.html")
+
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect(reverse("index"))
